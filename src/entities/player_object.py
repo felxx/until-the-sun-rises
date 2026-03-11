aa09a67b88f1b89573fbbffae3ac6a3c32096d1d
httpteruel <20230006488@estudantes.ifpr.edu.br>
2026-03-11 14:55:21 -0300

    feat: correção do sistema de movimentação para 360º, correção da fórmula de tiro, adição dos novos sprites

--- src/entities/player_object.py
+++ src/entities/player_object.py
@@ -1,53 +1,55 @@
 import pygame
+import math
+import os
 from src.entities.dynamic_object import DynamicObject
-
 
 class PlayerObject(DynamicObject):
     def __init__(self, x, y, speed, color, radius=15):
         super().__init__(x, y, speed, color, radius)
 
-        self.animations = {
-            "north": self.load_strip("assets/SMS_Soldier_WALK_NORTH_strip4.png"),
-            "south": self.load_strip("assets/SMS_Soldier_WALK_SOUTH_strip4.png"),
-            "east":  self.load_strip("assets/SMS_Soldier_WALK_EAST_strip4.png"),
-            "west":  self.load_strip("assets/SMS_Soldier_WALK_WEST_strip4.png"),
-        }
+        self.frames = []
+        for i in range(1, 10):
+            path = f"assets/rifle{i}.png"
+            try:
+                img = pygame.image.load(path).convert_alpha()
+                img = pygame.transform.scale(img, (64, 64))
+                self.frames.append(img)
+            except pygame.error:
+                surf = pygame.Surface((64, 64))
+                surf.fill((255, 0, 255))
+                self.frames.append(surf)
 
-        self.direction = "south"
         self.frame_index = 0
-        self.image = self.animations[self.direction][self.frame_index]
+        self.angle = 0
+        self.original_image = self.frames[self.frame_index]
+        self.image = self.original_image
         self.rect = self.image.get_rect(center=(x, y))
-
-    def load_strip(self, path):
-        full_sheet = pygame.image.load(path).convert_alpha()
-        full_sheet = pygame.transform.scale(
-            full_sheet, (full_sheet.get_width() * 3, full_sheet.get_height() * 3))
-        w = full_sheet.get_width() // 4
-        h = full_sheet.get_height()
-        return [full_sheet.subsurface(pygame.Rect(i*w, 0, w, h)) for i in range(4)]
 
     def resolve_behavior(self, dt):
         keys = pygame.key.get_pressed()
         self.velocity.x = keys[pygame.K_d] - keys[pygame.K_a]
         self.velocity.y = keys[pygame.K_s] - keys[pygame.K_w]
+        
+        if self.velocity.length() > 0:
+            self.velocity = self.velocity.normalize()
 
     def update(self, dt):
         super().update(dt)
-        self.rect.center = self.position
-
+        
         if self.velocity.length() > 0:
-            if abs(self.velocity.x) > abs(self.velocity.y):
-                self.direction = "east" if self.velocity.x > 0 else "west"
-            else:
-                self.direction = "south" if self.velocity.y > 0 else "north"
-
-            self.frame_index += 10 * dt
-            if self.frame_index >= 4:
+            self.frame_index += 12 * dt
+            if self.frame_index >= len(self.frames):
                 self.frame_index = 0
         else:
             self.frame_index = 0
 
-        self.image = self.animations[self.direction][int(self.frame_index)]
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        rel_x, rel_y = mouse_x - self.position.x, mouse_y - self.position.y
+        self.angle = math.degrees(math.atan2(-rel_y, rel_x)) - 90
+
+        self.original_image = self.frames[int(self.frame_index)]
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center=self.position)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
